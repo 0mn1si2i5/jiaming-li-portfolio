c7005a409772638f358ac951eeb51af4b99b8065
0mn1si2i5 <nomisomnis@gmail.com>
2026-07-29 02:26:10 +0800

    feat(portfolio): add OmniPet engine case study

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,6 +7,8 @@ export const site = {
     dialogTreePaper: 'https://doi.org/10.1145/3772363.3798792',
     mundusWebsite: 'https://0mn1si2i5.github.io/Mundus/',
     mundusGithub: 'https://github.com/0mn1si2i5/Mundus',
+    omnipetGithub: 'https://github.com/0mn1si2i5/OmniPet',
+    omnipetsGithub: 'https://github.com/0mn1si2i5/OmniPets',
     sideB: 'https://github.com/0mn1si2i5/Side-B',
     nbtiWebsite: 'https://0mn1si2i5.github.io/NBTI/',
     nbtiGithub: 'https://github.com/0mn1si2i5/NBTI',
